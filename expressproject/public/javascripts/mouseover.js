637a8a569cec5cd3fdbcf2b92f47f1aed3e8a77a
Geo1088 <georgej1088@gmail.com>
2017-04-14 00:18:47 -0400

    oh shit i fixed a thing

--- expressproject/public/javascripts/mouseover.js
+++ expressproject/public/javascripts/mouseover.js
@@ -1,9 +1,10 @@
 document.getElementById("nanashi").onmouseover = function() {mouseOver()};
 document.getElementById("nanashi").onmouseout = function() {mouseOut()};
 function mouseOver(){
+  console.log('hi')
 	document.getElementById('placeholder').src= 'http://i.imgur.com/YcMdHLJ.jpg';
 }
 function mouseOut(){
-	document.getElementById('placeholder').src= 'http://i.imgur.com/zKrhNaW.png' 
+  console.log('o ok bye')
+	document.getElementById('placeholder').src= 'http://i.imgur.com/zKrhNaW.png'
 }
-
