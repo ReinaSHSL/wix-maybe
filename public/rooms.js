4ee50f437b7ed773cb550dedaff6d61ba2ebd042
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2017-07-13 11:48:57 -0700

    im a bigger genius

--- public/rooms.js
+++ public/rooms.js
@@ -6,18 +6,18 @@ $('#create').click(function(){
 	var roomId = Math.random()
 	//console.log(roomId)
     socket.emit("createRoom", roomId)
-    $('#roomList').append('<li>' + roomId + '</li>')
+    $('#roomList').append('<li id = ' + i + '>' + '<a href=#>' + roomId + '</a>' + '</li>')
 })
 //Tells other clients to display room
 socket.on('newRoom', function(newRoom){
-    $('#roomList').append('<li>' + newRoom + '</li>')
+    $('#roomList').append('<li id = ' + i + '>' + '<a href=#>' + newRoom + '</a>' + '</li>')
 })
 //On connection list all active rooms
 socket.on('activeRooms', function(activeRooms){
 	var roomList = $('#roomList')
     roomList.empty()
     for(i of activeRooms){
-        roomList.append('<li>' + i + '</li>')
+        roomList.append('<li id = ' + i + '>' + '<a href=#>' + i + '</a>' + '</li>')
     }
 })
 
