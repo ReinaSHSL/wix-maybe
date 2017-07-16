683776e6ee5b0217fb302a55beb4043b868047d8
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2017-07-16 12:34:47 -0700

    nitpicker commit

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ io.on('connection', function (socket) {
         var idIndex = activeRooms[socket.room].ids.indexOf(socket.id)
         activeRooms[socket.room].ids.splice(idIndex, 1)
         console.log(activeRooms)
-        if(activeRooms[socket.room].ids.length){
+        if (activeRooms[socket.room].ids.length){
             if (socket.id === activeRooms[socket.room].roomLeader){
                 if (activeRooms[socket.room].ids.length) {
                     activeRooms[socket.room].roomLeader = ''
