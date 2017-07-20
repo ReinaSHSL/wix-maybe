72dddde31eb5cf9aa2c9bfcd2dff09661c46dcbc
Geo1088 <georgej1088@gmail.com>
2017-07-20 00:16:55 -0400

    Restyling a bunch of stuff woo

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
+// TODO: User class, have the rooms only store the ID, this will let us do
+// actions in a room when a person changes usernames and stuff
 
 let rooms = []
 class Room {
