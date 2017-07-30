26fbe6d13adc2e3b3f3a91fdde178ae53f7d82c5
Geo1088 <georgej1088@gmail.com>
2017-07-29 21:12:08 -0400

    Handle login and signup a little better

--- public/js/accounts.js
+++ public/js/accounts.js
@@ -3,7 +3,9 @@
 $('.login-form, .signup-form').on('submit', function (e) {
     e.preventDefault()
     const $this = $(this)
-    $this.find('input[type="submit"]').attr('disabled', true)
+    const $submitButton = $this.find('input[type="submit"]')
+    // Disable to avoid duping request - enable when we get a response
+    $submitButton.attr('disabled', true)
     $.ajax({
         url: $this.attr('action'),
         type: $this.attr('method'),
@@ -13,16 +15,21 @@ $('.login-form, .signup-form').on('submit', function (e) {
             if ($this.is('.login-form')) {
                 alert('logged in!')
                 $('.current-user').text()
+                $submitButton.attr('disabled', false)
+                $('.current-user').text($('.login-form [name="username"]').val())
             } else {
                 alert('signed up! please log in now')
+                $submitButton.attr('disabled', false)
             }
         },
         error: function (response) {
             // const data = JSON.parse(response)
             if ($this.is('.login-form')) {
-                alert('failed to log in...')
+                alert('failed to log in...\n' + response.responseText)
+                $submitButton.attr('disabled', false)
             } else {
-                alert('failed to sign up...')
+                alert('failed to sign up...\n' + response.responseText)
+                $submitButton.attr('disabled', false)
             }
         }
     })
