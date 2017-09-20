c65d8784db2533102a83108a0fea1da70c3ce909
George Platko <georgej1088@gmail.com>
2017-09-20 18:56:33 +0000

    Database setup script; package.json inprovements
    - Create dbinit.js to set up the database on first run
    - Get rid of old dependency that's no longer used
    - Add npm scripts to package.json
      - "c9-start" runs the script with ports set up for Cloud9 environment
      - "rethinkdb" runs rethinkdb with --bind set to all and --port set to $PORT or 3000 (the default)

---
+++ dbinit.js
@@ -0,0 +1,49 @@
+const r = require('rethinkdb')
+const dbConfig = require('./dbConfig')
+
+const dbName = 'batoru'
+const dbTables = [
+    'selectors'
+]
+
+r.connect(dbConfig, function (err, conn) {
+    if (err) return console.log(err)
+    console.log(`[db-init] RethinkDB connection on port ${dbConfig.port}.`)
+    r.dbList().run(conn, (err, dbs) => {
+        if (err) return console.log(err)
+        if (dbs.indexOf(dbName) < 0) {
+            r.dbCreate(dbName).run(conn,(err, result) => {
+                if (err) return console.log(err)
+                console.log(`[db-init] Database '${dbName}' created.`)
+                checkTables()
+            })
+        } else {
+            console.log(`[db-init] Database '${dbName}' found.`)
+            checkTables()
+        }
+
+        // welcome to async hell
+        function checkTables () {
+            r.db(dbName).tableList().run(conn, (err, tables) => {
+                if (err) return console.log(err)
+                ! function checkTable (i = 0) {
+                    const name = dbTables[i]
+                    if (!name) {
+                        console.log('[db-init] Done, exiting.')
+                        process.exit()
+                    }
+                    if (tables.indexOf(name) < 0) {
+                        r.db(dbName).tableCreate(name).run(conn, (err) => {
+                            if (err) return console.log(err)
+                            console.log(`[db-init] Table '${name}' created.`)
+                            checkTable(++i)
+                        })
+                    } else {
+                        console.log(`[db-init] Table '${name}' found.`)
+                        checkTable(++i)
+                    }
+                }()
+            })
+        }
+    })
+})
